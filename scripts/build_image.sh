29fc59beae43c01c2361c41ce89edb2beb12788d
Akshay.Patil <akshay.patil@pubmatic.com>
2021-07-21 17:46:52 +0530

    path change

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -3,7 +3,5 @@ echo "*****************"
 echo "*** Building ****"
 echo "*****************"
 
-WORKSPACE=/home/akshay/Documents/jenkins/jenkins_home/workspace/pipeline-demo
-pwd
-
+WORKSPACE=/var/jenkins_home/workspace/pipeline-demo
 docker build -t akki/angular-app $WORKSPACE/demo-app
