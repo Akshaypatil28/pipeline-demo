94fa6ad77081e0b62cb0aa6cc313604a728daeb4
Akshay.Patil <akshay.patil@pubmatic.com>
2021-07-21 17:44:21 +0530

    tmp

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -4,4 +4,5 @@ echo "*** Building ****"
 echo "*****************"
 
 WORKSPACE=/home/akshay/Documents/jenkins/jenkins_home/workspace/pipeline-demo
+cat ./build_image.sh
 docker build -t akki/angular-app $WORKSPACE/demo-app
