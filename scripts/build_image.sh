97e67324f906fb78b8a394350c8ae7db80102c26
Akshay.Patil <akshay.patil@pubmatic.com>
2021-07-21 19:50:07 +0530

    running server

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -5,3 +5,5 @@ echo "*****************"
 
 WORKSPACE=/var/jenkins_home/workspace/pipeline-demo
 docker build -t akki/angular-app $WORKSPACE/demo-app
+
+docker run -d -it -p 8895:80/tcp --name angular-app-demo-$COUNTER akki/angular-app
